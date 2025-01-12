c8cf48aa221131affd6cdea224d007a8c8850259
mac <magicaltitan7@gmail.com>
2025-01-12 19:39:24 -0400

    Removed InstallerLauncher.java

--- src/main/java/org/vermac/textuallyyoursinstaller/InstallerLauncher.java
+++
@@ -1,9 +0,0 @@
-package org.vermac.textuallyyoursinstaller;
-
-import javafx.application.Application;
-
-public class InstallerLauncher {
-    public static void main(String[] args) {
-        Application.launch(InstallerApplication.class);
-    }
-}
