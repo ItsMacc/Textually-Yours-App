a2cb666fd6a024af0a2afb99c01b44a0977960dd
mac <magicaltitan7@gmail.com>
2025-01-12 19:41:15 -0400

    Fixed Bugs

--- src/main/java/org/vermac/textuallyyours/AppLauncher.java
+++ src/main/java/org/vermac/textuallyyours/AppLauncher.java
@@ -5,11 +5,9 @@ import javafx.application.Application;
 import org.vermac.textuallyyoursinstaller.InstallerApplication;
 
 public class AppLauncher {
-    public static boolean installed = false;
-
     public static void main(String[] args) {
 
-        if (AppStateManager.canStartApp() && installed) {
+        if (AppStateManager.canStartApp()) {
             Application.launch(TextuallyYoursApplication.class);
         } else {
             Application.launch(InstallerApplication.class);
